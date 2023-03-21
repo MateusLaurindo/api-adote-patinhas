0b7d60ac25ec536422a090917ef939e80abfe020
MateusLaurindo <102548678+MateusLaurindo@users.noreply.github.com>
2023-03-21 13:58:46 -0300

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
